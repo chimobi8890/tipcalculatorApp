69ae8212400b1dce98f66bd8654342c67b3bef27
Chimobi Ucha <chimobi.ucha@gmail.com>
2020-09-12 22:02:15 -0400

    Included a settings page

---
+++ tip/SettingsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsViewController.swift
+//  tip
+//
+//  Created by Chimobi Ucha on 9/8/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Settings"
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       // Do any additional setup after loading the view.
+        
+        // Sets the title in the Navigation Bar
+        self.title = "Tip Calculator"
+       
+        //makes the bill amount textfield the first responder when app loads
+        billAmountTextField.becomeFirstResponder()
+       
+        //changes keyboard type to numeric
+        billAmountTextField.keyboardType = .numberPad
+
     }
-
-
+    
+    
+    @IBAction func onTap(_ sender: Any) {
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+    //Get initial bill amount and calculate tips
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.2] 
+        
+    //Calculate tip and total
+        let tip = bill * tipPercentages [tipControl.selectedSegmentIndex]
+        let total = bill + tip
+    
+        
+    // Update the tip and total labels
+        tipPercentageLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
+
 
